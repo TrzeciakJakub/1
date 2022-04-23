cdb8a37ed1a4e85a40b69ebc8c70b9cb0c85b6b0
TrzeciakJakub <trzeciakjakub@gmail.com>
2022-04-23 17:00:29 +0200

    zmiany 17.00 23.04

--- 1.py
+++ 1.py
@@ -1,3 +1,6 @@
 print('Hello world')
 print('Druga Instrukcja')
-
+#x = 3 #integer
+#f = 3.14 #float
+#name = "Python"
+#print(f)
---
+++ start3.py
@@ -0,0 +1,25 @@
+print('Hello World')
+
+x = 3 #integer
+f = 3.24 #float 
+name = "Python" #string 
+arr = ["apple", "banana", "cherry"] #list   
+
+dict = {"name" : "John", "age" : 36, "maria" : 33} #dict                                
+
+logic = True #boolean   
+
+name = name[1:4]
+
+x = 1/3
+sum = x + f     
+age = str(dict["age"])  
+johnAge = age + "John"  
+age = int(age) + 1      
+maria = list(dict.values())[2]   
+print(age)
+print(dict["name"] + "John")
+#sumInteger = (int(x) + int(f))
+print(sum)  
+#print(shortName)
+#print(sumInteger)
