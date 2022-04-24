166cec74bc3dec5805cbe9366d1a3c6569db579d
TrzeciakJakub <trzeciakjakub@gmail.com>
2022-04-24 08:59:47 +0200

    zmiany 24.04 8.00

--- start3.py
+++ start3.py
@@ -1,16 +1,12 @@
 print('Hello World')
-print("Kuba Trzeciak")
+print("Kuba Trzeciak 2")
 x = 3 #integer
 f = 3.24 #float 
 name = "Python" #string 
 arr = ["apple", "banana", "cherry"] #list   
-
 dict = {"name" : "John", "age" : 36, "maria" : 33} #dict                                
-
 logic = True #boolean   
-
 name = name[1:4]
-
 #x = 2/3
 sum = x + f     
 age = str(dict["age"])  
@@ -19,9 +15,11 @@ age = int(age) + 1
 maria = list(dict.values())[2]   
 print(age)
 print(maria)
+print(list(arr))
 print(johnAge)
 print(dict["name"] + "John")
-#sumInteger = (int(x) + int(f))
+sumInteger = (int(x) + int(f))
 print(sum)  
 #print(shortName)
-#print(sumInteger)
+print(sumInteger)
+ 
