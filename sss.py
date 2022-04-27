183188a66e56a0bdbb15c11700a3860848e45ade
TrzeciakJakub <trzeciakjakub@gmail.com>
2022-04-27 18:52:38 +0200

    27.04.2022 19:00

--- sss.py
+++ sss.py
@@ -1,68 +1,68 @@
 import random
 
+# MENU 
 print("")
-tekst = "HangMan Game\n"
+tekst = "Hangman Game\n"
 print(tekst.center(80))
 tekst2 = "Wybierz Poziom Trudnosci\n"
 print(tekst2.center(80))
 print("1 = Latwy   \n2 = Sredni \n3 = Trudny \n")
 
-
-def is_number(userInput):
-    value = str(userInput)
-    if value.isdecimal(): 
-    #value = int(userInput)
-    #if (value >=1 and value <=3):
+#prosba o podanie poziomu trudnosci 1 2 lub 3  - ale jak wpisze litere to wywala bledy 
+def is_number(a):
+    #value = str(userInput)
+    #if value.isdecimal(): 
+    value = int(a)
+    if (value >=1 and value <=3):
         return True
-    else: 
+    elif value.isdecimal():
+        return True
+    else:
         return False
-    pass
+pass
 
 
-def ask_for_a_difficulty(forceValidInput):
+def ask_for_a_difficulty(b):
     while True:
         a = input("Wybieram Poziom: ")
-        a_difficulty = is_number(a)
-        if a_difficulty is False:
-            print("Nieprawidlowy wybor- musisz wybrac 1/2/3")
-        else:
+        ask_for_a_difficulty = is_number(a)
+        if ask_for_a_difficulty is True:
             print("Wybrales poziom: %s  ------>>> Zaczynamy!"%a)
+        else:
+            print("Nieprawidlowy wybor- musisz wybrac 1/2/3")
+        #if ask_for_a_difficulty is False:
+        #   print("Nieprawidlowy wybor- musisz wybrac 1/2/3")
+        #else:
+        #    print("Wybrales poziom: %s  ------>>> Zaczynamy!"%a)
+        break    
 
-words = ['cat', 'dog','zebra','whale','doplhin']
-def pick_a_word():
-    word_position = random.randint(0, len(words) -1)
-    return words[word_position]
-print(pick_a_word())         
+
+#wyrzucanie slowa jako _ _ _ _ _
+#words = ['cat', 'dog','zebra','whale','dolphin', 'giraffe', 'at']
+#def pick_a_word():
+#    word = random.choice(words).upper()
+#    new_word = word 
+#    for i in range(0, len(word)):
+#        new_word = new_word.replace(word[i], ' _ ')
+#    return(new_word)  
+#print(pick_a_word())
+#input("Odgadnij Slowo:")       
 
 
 
-def menu():
+def main():
     while True:
-        a = ask_for_a_difficulty(True)
-        if not a:
+         a = ask_for_a_difficulty(True)
+         
+         if not a:
             break
+        
        
 
     pass
 if __name__ == '__main__':
-    menu()
-
-    
-
-#words = ["dog", "cat", "elephant", "rabbit", "mouse", "horse", "hamster", "giraffe", "snake", "gepard", "duck", "antelope", "hippo", "apple", "pizza", "strawberry", "milk", "spaghetti", "mayonnaise", "juice", "chocolate", "scissors", "ball", "computer", "headphones", "microwave", "fridge", "programmer", "hairdresser", "doctor", "engineer", "restaurant", "library", "museum", "cinema", "game", "guitar", "bike", "skateboard", "piano"]
-
-#def get_world(words):
-#    word = random.randint(0, len(words) -1)
-#    print(word)
+    main()
 
 
 
-#def menu():
-#    while True:
-#        a = ask_for_a_difficulty(True)
-        
-       
-
-#    pass
-#if __name__ == '__main__':
-#    menu()
+############################################################
