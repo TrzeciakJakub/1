ad2ba54d05edba67591b625d5da521bfd4eb0df7
TrzeciakJakub <trzeciakjakub@gmail.com>
2022-05-05 16:10:46 +0200

    05.05

--- robocze nauka.py
+++ robocze nauka.py
@@ -413,16 +413,112 @@
 # print(abs(42))
 # print(sum([1, 2, 3, 4, 5]))
 
-filename = input("Enter a filename:")
-with open(filename) as f:
-    text = f.read()
-print(text)
+# filename = input("Enter a filename:")
+# with open(filename) as f:
+#     text = f.read()
+# print(text)
 
 
-def count_char(text, char):
-    count = 0 
-    for c in text: 
-        if c == char:
-            count += 1
-    return count
-print(count_char(text, "c"))      
+# def count_char(text, char):
+#     count = 0 
+#     for c in text: 
+#         if c == char:
+#             count += 1
+#     return count
+# print(count_char(text, "c"))      
+
+# def apply_twice(func, arg):
+#     return func(func(arg))
+
+# def add_five(x):
+#     return x + 5
+
+# print(apply_twice(add_five,10))
+
+#def my_func(f, arg):
+#    return f(arg)
+#my_func(lambda x : 2*x*x, 5)
+#print()
+
+# def add_five(x):
+#     return x + 5
+# nums = [11, 22, 33, 44, 55]
+# result = list(map(add_five, nums))
+# print(result)
+
+# nums = [11, 22, 33, 44, 55]
+# result = list(map(lambda x: x+5, nums))
+# print(result)
+
+# nums = [11, 22, 33, 44, 55]
+# res = list(filter(lambda x: x%2 ==0, nums))
+# # print(res)
+
+# def countdown():
+#     i=5
+#     while i > 0:
+#         yield 1
+#         i -= 1
+
+# for i in countdown():
+#     print(i)
+
+# def numbers(x):
+#     for i in range(x):
+#         if i % 2 == 0:
+#             yield i
+# print(list(numbers(11)))
+
+# def decor(func):
+#     def wrap():
+#         print("=====")
+#         func()
+#         print("=====")
+#     return wrap
+# def print_text():
+#     print("Hello world!")
+# decorated = decor(print_text)
+# decorated()
+
+
+#def factorial(x):
+#    if x == 1:
+#        return 1
+#    else:
+#        return x * factorial(x-1)
+#print(factorial(5))
+
+# def is_even(x):
+#     if x == 0:
+#         return True
+#     else:
+#         return is_odd(x-1)
+
+# def is_odd(x):
+#     return not is_even(x)
+
+# print(is_odd(17))
+# print(is_even(23))
+
+# num_set = {1, 2, 3, 4, 5}
+# word_set = set(["spam", "eggs", "sausage"])
+
+# print(3 in num_set)
+# print("spam" not in word_set)
+
+# nums = {1, 2, 1, 3, 1, 4, 5, 6}
+# print(nums)
+# nums.add(-7)
+# nums.remove(3)
+# print(nums)
+
+
+first = {1, 2, 3, 4, 5, 6}
+second = {4, 5, 6, 7, 8, 9}
+
+print(first | second)
+print(first & second)
+print(first - second)
+print(second - first)
+print(first ^ second)
+
